7ed89bb73055b75fa304e322b09f7c7c603b4311
Giselle Reis <giselle.mnr@gmail.com>
2017-05-04 14:27:29 +0300

    Modified script for generating new library

--- run-parser.sh
+++ run-parser.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Run with:
-# ./run-parser.sh TPLib/Problems/
+# ./run-parser.sh TPLib/Problems/ target_dir
 
 dir=$1
+target=$2
+
+mkdir $target
 
 for f in `find $dir -name "*.p"`; do
 
   echo "Processing $f"
-  ./tptpparser/tptpparser $f
+  problem=`basename $f`
+  category=`basename $(dirname $f)`
+  # Create directory if it does not exsit
+  mkdir -p $target/$category
+  ./tptpparser/tptpparser $f > $target/$category/$problem
 
 done
