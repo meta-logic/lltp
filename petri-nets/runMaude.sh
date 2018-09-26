2e5b1054d81c677ba974b83f4368e79f1554a072
Giselle Reis <giselle.mnr@gmail.com>
2018-09-26 14:01:27 +0300

    runMaude: instructions and finding one state

--- petri-nets/runMaude.sh
+++ petri-nets/runMaude.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-steps=4
+# This script runs maude on a file to find the reachable states after a certain
+# number of steps, and, for each such state, creates a complete LLTP problem.
+# Theu maude files must be in a directory called mcc-maude and are processed in
+# increasing order of size.
+# The corresponding partial LLTP problems are in a directory called mcc-lltp.
+# The LLTP files are stored in a directory called MCC.
+#
+# Giselle Reis - 2018
+
+steps=( 5 20 50 100 )
 mkdir MCC
 
 # Processing the files from smallest to biggest
@@ -9,24 +18,33 @@ for f in `ls -Sr mcc-maude/*`; do
   bn=$(basename $f)
   root="${bn%.*}"
   lltp_file=mcc-lltp/${root}.lltp.part
-  results=$(echo "red successor(${steps}, init) ." | maude.linux64 -no-wrap -batch $f  | grep result | sed 's/result SMarking: //g ; s/(//g ; s/)//g' | tr ",," "\n")
+
+  for s in "${steps[@]}"; do
+
+    # Finds all reachable states
+    #results=$(echo "red successor(${steps}, init) ." | maude.linux64 -no-wrap -batch $f  | grep result | sed 's/result SMarking: //g ; s/(//g ; s/)//g' | tr ",," "\n")
+
+    # Finds one reachable state
+    results=$(echo "red benchRewrite(${s}, init) ." | maude -no-wrap -batch $f  | grep result | sed 's/result \(EmptyCommaList\|Marking\): //g' )
   
-  if [ "$results" != "empSM" ]; then
-
-    counter=1
-    for r in $results; do
-
-      echo " Result ${counter}: $r"
-
-      file=MCC/${root}_${steps}_${counter}.p
-      echo " Writing file $file"
-
-      cat $lltp_file > ${file}
-      conj=$(echo $r  | tr -d "'" | sed 's/ / \* /g')
-      echo "fof(con1, conjecture, ${conj})." >> ${file}
-      counter=$((counter+1))
-    done
-  else
-    echo " No reachable state."
-  fi
+    if [ "$results" != "empSM" ] && [ "$results" != "empty" ]; then
+ 
+      counter=1
+      IFS=$'\n'
+      for r in $results; do
+ 
+        echo " Result ${counter}: $r"
+ 
+        file=MCC/${root}_${s}_${counter}.p
+        echo " $s steps: writing file $file"
+ 
+        cat $lltp_file > ${file}
+        conj=$(echo $r  | tr -d "'" | sed 's/ / \* /g')
+        echo "fof(con1, conjecture, ${conj})." >> ${file}
+        counter=$((counter+1))
+      done
+    else
+      echo " $s steps: no reachable state."
+    fi
+  done
 done
